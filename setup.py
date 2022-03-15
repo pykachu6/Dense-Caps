08aacdddd561d52b539c2b3ed2e2cb77f19b7c22
pykachu6 <pykachu6@uw.edu>
2022-03-15 12:27:03 -0700

    changeing setup

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 import setuptools
 
-
-setuptools.setup(
-    name='crystpredict',
-    version='0.0.31',
-    author='Dunce Caps',
-    author_email='pykachu6@uw.edu',
-    description='Crystal predictor',
-    url='https://github.com/pykachu6/Dunce-Caps',
-    license='MIT',
-    packages=['toolbox'],
-)
+setuptools.setup()
