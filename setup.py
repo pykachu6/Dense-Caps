96d470e0031329e93b6ecbfa6abbc68a18a273ee
pykachu6 <pykachu6@uw.edu>
2022-03-15 12:16:07 -0700

    changeing setup

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name='crystpredict',
@@ -9,8 +7,6 @@ setuptools.setup(
     author='Dunce Caps',
     author_email='pykachu6@uw.edu',
     description='Crystal predictor',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url='https://github.com/pykachu6/Dunce-Caps',
     license='MIT',
     packages=['toolbox'],
